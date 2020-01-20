2ca1d8c5f1f04f1167198b4a7b580c20dc4d2b0b
곽근열 <piakpiak9@naver.com>
2020-01-20 13:21:52 +0900

    created new branch

--- frontend-vue/vue/src/main.js
+++ frontend-vue/vue/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import router from 'vue-router';
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
