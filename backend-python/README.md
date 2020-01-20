db1c746d00d2e94525b22793972f190fc3055a17
Myeonghan-Jeong <ataraxia9408@gmail.com>
2020-01-20 10:07:45 +0900

    init basic python backend

---
+++ backend-python/README.md
@@ -0,0 +1,11 @@
+# 거기어디니? backend python
+
+## 공통프로젝트 2주차(200120 ~ 200123)
+
+### 이번 주에 할 일
+
+1. django 기본 세팅 진행
+
+2. django에 mysql 연결
+
+3. django instagram crawling 테스트
---
+++ backend-python/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\multicampus\study_in_ssafy\backend-python\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
