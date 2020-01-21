487f97a6444afd7ec180f8136fa325a92ce2fa50
곽근열 <piakpiak9@naver.com>
2020-01-21 15:52:10 +0900

    crawling test page(prototype, under construct)

--- frontend-vue/vue/src/App.vue
+++ frontend-vue/vue/src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div v-for="(item3, index) in imgs" :key="index">
+        pcode : {{index+1}}
+        <div v-for="(itemi,idx) in item3" :key="idx">  
+          <img :src="itemi">
+        </div>
+      </div>
+      <a @click="ax">hi</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name:'app',
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+    src: null,
+    arr: null,
+    imsrg: null,
+    length: 100,
+    tmp: [],
+    imgs: [],
+  }),
+  methods:{
+    ax() {
+      axios
+      .get('http://127.0.0.1:8000/instagram/?length='+this.length+'&target=tour') //여기에 url이 들어갑니다
+      .then(response => {
+        this.arr = response.data.purl;
+        this.imsrg = response.data.images;
+        this.src = response.data;
+        for (let index = 1; index < 100; index++) {
+          this.tmp.push(response.data[index])
+        }
+        for (let index = 0; index < 99; index++) {
+          this.imgs.push(this.tmp[index]['purl'])
+        }
+      })
+    }
+  },
+  mounted(){
+    this.ax;
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
