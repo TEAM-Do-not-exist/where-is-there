c719ae548ae74e606e2e2a25eca328864090ed7f
ohjiyun <orora5411@naver.com>
2020-01-22 09:36:42 +0900

    S02P12C107-58  채팅방 front붙이기 완료

--- frontend-vue/vue/src/main.js
+++ frontend-vue/vue/src/main.js
@@ -5,12 +5,18 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import './plugins/socketPlugin';
+import Directives from './plugins/directives';
+import store from './store';
 
 Vue.config.productionTip = false
+Vue.use(Directives);
 
 new Vue({
   router,
   vuex,
   vuetify,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
