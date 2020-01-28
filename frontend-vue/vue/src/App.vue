707047a03cccc24e2e14f70fa240aaf0218c8c42
ohjiyun <orora5411@naver.com>
2020-01-28 16:17:53 +0900

    채팅 아이콘과 app.vue 코드정리

--- frontend-vue/vue/src/App.vue
+++ frontend-vue/vue/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <v-app id="inspire">
-   <v-navigation-drawer
-      v-model="drawerRight"
-      app
-      right
-    >
+    <v-navigation-drawer v-model="drawerRight" app right>
       <v-list dense>
-          <v-list-item-content>
+        <v-list-item-content>
           <div class="inner-wrap" fluid fill-height inner-wrap>
             <Message-List :msgs="msgDatas" class="msg-list"></Message-List>
             <Message-From v-on:submitMessage="sendMessage" class="msg-form"></Message-From>
           </div>
-          </v-list-item-content>
+        </v-list-item-content>
       </v-list>
     </v-navigation-drawer>
-      
-    <v-navigation-drawer v-model="drawer" class=".d-flex" 
-    temporary app
-    style="z-index:1">
+
+    <v-navigation-drawer v-model="drawer" class=".d-flex" temporary app style="z-index:1">
       <v-container>
-        <img
-          width="80"
-          @click="to_home"
-          src="https://lab.ssafy.com/uploads/-/system/appearance/header_logo/1/ssafy_logo.png"
-        />
-        <!-- <h3>
-          거기 어디니?
-        </h3>-->
+        <img width="80" @click="to_home"
+          src="https://lab.ssafy.com/uploads/-/system/appearance/header_logo/1/ssafy_logo.png" />
       </v-container>
       <v-list dense>
         <v-list-item @click="to_home">
@@ -68,26 +56,14 @@
 
     <v-app-bar app light flat>
       <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
-           <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
-      <i class="fa fa-camera-retro"></i>
-      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon>
+      <i class="far fa-comments fa-2x" @click.stop="drawerRight = !drawerRight"></i>
+      <!-- <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon> -->
     </v-app-bar>
 
     <v-content>
       <router-view />
-      <!-- <div>
-      <v-btn
-        fixed
-        top
-        left
-        class="elevation-0"
-        large
-        @click.stop="drawer = !drawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-      </div>-->
     </v-content>
 
     <v-footer color="blue" app>
@@ -98,126 +74,125 @@
         </v-btn>
       </v-fab-transition>
     </v-footer>
-
-      <!-- <div class="inner-wrap" fluid fill-height inner-wrap
-        style="position: absolute; right:0%; width:500px; height:1200px;">
-    <Message-List :msgs="msgDatas" class="msg-list"></Message-List>
-    <Message-From v-on:submitMessage="sendMessage" class="msg-form"></Message-From>
-  </div> -->
   </v-app>
 </template>
 
 <script>
-import * as easings from "vuetify/es5/services/goto/easing-patterns";
+  import * as easings from "vuetify/es5/services/goto/easing-patterns";
+  import 'expose-loader?$!expose-loader?jQuery!jquery';
+  import axios from "axios";
+  import {
+    mapMutations,
+    mapState
+  } from "vuex";
+  import MessageList from "@/components/Chat/MessageList.vue";
+  import MessageForm from "@/components/Chat/MessageForm.vue";
+  import Constant from "@/Constant";
+  import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
 
-import axios from "axios";
-import { mapMutations, mapState } from "vuex";
-import MessageList from "@/components/Chat/MessageList.vue";
-import MessageForm from "@/components/Chat/MessageForm.vue";
-import Constant from "@/Constant";
-
-export default {
-  props: {
-    source: String
-  },
+  export default {
+    icons: {
+      iconfont: 'fa',
+    },
+    props: {
+      source: String
+    },
     name: "ChatRoom",
- 
-  data: () => ({
-    drawer: null,
-    drawerRight: null,
-    right: false,
-    left: false,
-    src: null,
-    easing: "easeInOutCubic",
-    easings: Object.keys(easings),
-    enter: true,
-    
-  }),
+
+    data: () => ({
+      drawer: null,
+      drawerRight: null,
+      right: false,
+      left: false,
+      src: null,
+      easing: "easeInOutCubic",
+      easings: Object.keys(easings),
+      enter: true,
+
+    }),
     components: {
-    "Message-List": MessageList,
-    "Message-From": MessageForm
-  },
-  mounted() {
-    axios
-      .get("") //여기에 url이 들어갑니다
-      .then(response => {
-        this.data.src = response;
-      });
-  },
-  computed: {
-     ...mapState({
-      msgDatas: state => state.socket.msgDatas
-    }),
-    options() {
-      return {
-        duration: 300,
-        offset: 0,
-        easing: 0
-      };
+      "Message-List": MessageList,
+      "Message-From": MessageForm
     },
-    
-  },
+    mounted() {
+      axios
+        .get("") //여기에 url이 들어갑니다
+        .then(response => {
+          this.data.src = response;
+        });
+    },
+    computed: {
+      ...mapState({
+        msgDatas: state => state.socket.msgDatas
+      }),
+      options() {
+        return {
+          duration: 300,
+          offset: 0,
+          easing: 0
+        };
+      },
+
+    },
     created() {
-    const $ths = this;
-    this.$socket.on("chat", data => {
-      this.pushMsgData(data);
-      $ths.datas.push(data);
-    });
-  },
-  methods: {
-    to_home() {
-      this.$router.push("/");
-    },
-    to_sign_in() {
-      this.$router.push("/signin");
-    },
-    to_test() {
-      this.$router.push("/test");
-    },
-    to_chat() {
-      this.$router.push("/chat");
-    },
-     ...mapMutations({
-      pushMsgData: Constant.PUSH_MSG_DATA
-    }),
-    sendMessage(msg) {
-      this.pushMsgData({
-        from: {
-          name: "나:"
-        },
-        msg
+      const $ths = this;
+      this.$socket.on("chat", data => {
+        this.pushMsgData(data);
+        $ths.datas.push(data);
       });
-      this.$sendMessage({
-        // name: this.$route.params.username,
-        name:'mymy',
-        msg
-      });
+    },
+    methods: {
+      to_home() {
+        this.$router.push("/");
+      },
+      to_sign_in() {
+        this.$router.push("/signin");
+      },
+      to_test() {
+        this.$router.push("/test");
+      },
+      to_chat() {
+        this.$router.push("/chat");
+      },
+      ...mapMutations({
+        pushMsgData: Constant.PUSH_MSG_DATA
+      }),
+      sendMessage(msg) {
+        this.pushMsgData({
+          from: {
+            name: "나:"
+          },
+          msg
+        });
+        this.$sendMessage({
+          // name: this.$route.params.username,
+          name: 'mymy',
+          msg
+        });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
-/* .theme--light.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined){
-  background-color : #fff0;
-} */
-.theme--light.v-sheet {
-  background-color: #fff0;
-}
+  .theme--light.v-sheet {
+    background-color: #fff0;
+  }
 </style>
 <style>
-.msg-form {
-  bottom: -28px;
-  position: absolute;
-  left: 0;
-  right: 0;
-}
-.msg-list {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 60px;
-  overflow-x: scroll;
-}
+  .msg-form {
+    bottom: -28px;
+    position: absolute;
+    left: 0;
+    right: 0;
+  }
+
+  .msg-list {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 60px;
+    overflow-x: scroll;
+  }
 </style>
