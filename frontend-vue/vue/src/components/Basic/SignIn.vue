b23a86a6087e928ea53c2caf4d734459658f8432
곽근열 <piakpiak9@naver.com>
2020-01-29 11:36:02 +0900

    S02P12C107-75

--- frontend-vue/vue/src/components/Basic/SignIn.vue
+++ frontend-vue/vue/src/components/Basic/SignIn.vue
@@ -1,54 +1,70 @@
 <template>
   <v-app id="inspire">
     <v-container>
-      <v-card flat>
-        <v-card-title>
-          <h1>SIGN IN</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-              v-model="id"
-              :rules="nameRules"
-              :counter="10"
-              label="ID"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="password"
-              :rules="passwordRules"
-              label="Password"
-              type="password"
-              required
-            ></v-text-field>
-            <v-container>
-              <KakaoLogin
-                api-key="6276da62b0eea6dd696fe9b1e192dfdb"
-                image="kakao_account_login_btn_large_wide_ov"
-                :on-success="onSuccess"
-                :on-failure="onFailure"
-              />
-            </v-container>
-            <v-container>
-              <NaverLogin
-                client-id="nsrxqIjEGhhBf9jdPBFD"
-                callback-url="/main"
-                v-bind:is-popup="true"
-                v-bind:button-type="3"
-                v-bind:button-height="100"
-                button-color="green"
-                :callbackFunction="callbackFunction"
-              />
-            </v-container>
-            <p>
-              <a @click="to_sign_up">sign up</a>
-            </p>
-            <v-btn :disabled="!valid" @click="submit" color="indigo" right>
-              Sign in
-            </v-btn>
-          </v-form>
-        </v-card-text>
-      </v-card>
+      <v-layout
+        align-center
+        justify-center
+      >
+          <v-card flat>
+            <v-card-title>
+              <h1>SIGN IN</h1>
+            </v-card-title>
+            <v-card-text>
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field
+                  v-model="id"
+                  :rules="nameRules"
+                  :counter="10"
+                  label="ID"
+                  required
+                  @keyup.13="submit"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :rules="passwordRules"
+                  label="Password"
+                  type="password"
+                  required
+                  @keyup.13="submit"
+                ></v-text-field>
+                
+                <v-layout
+                  align-center
+                  justify-center
+                >
+                  <KakaoLogin
+                    api-key="6276da62b0eea6dd696fe9b1e192dfdb"
+                    image="kakao_account_login_btn_medium_narrow"
+                    :on-success="onSuccess"
+                    :on-failure="onFailure"
+                  />
+                </v-layout>
+                <v-layout
+                  align-center
+                  justify-center
+                >
+                  <NaverLogin
+                    client-id="nsrxqIjEGhhBf9jdPBFD"
+                    callback-url="/main"
+                    v-bind:is-popup="true"
+                    v-bind:button-type="3"
+                    v-bind:button-height="60"
+                    button-color="green"
+                    :callbackFunction="callbackFunction"
+                  />
+                </v-layout>
+                <v-layout
+                  align-center
+                  justify-center
+                >
+                  <v-btn @click="to_sign_up" outlined width="277.33" height="60">
+                    ...or sign up now!
+                  </v-btn>
+                </v-layout>
+              </v-form>
+            </v-card-text>
+          </v-card>
+      </v-layout>
     </v-container>
   </v-app>
 </template>
@@ -132,5 +148,15 @@ export default {
 <style>
 span {
   display: inline;
+}
+#kakao-login-btn{
+  display: inline;
+}
+#kakao-login-btn img{
+  height: 60px;
+  width: 277.33px;
+}
+.v-btn__content {
+  font-size: 18px;
 }
 </style>
