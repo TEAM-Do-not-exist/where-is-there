24b3c79f5a40d7cca5c66175d08e91b9772e4137
Myeonghan-Jeong <ataraxia9408@gmail.com>
2020-01-29 16:30:45 +0900

    S02P12C107-63 add main page vue

--- frontend-vue/vue/src/store/index.js
+++ frontend-vue/vue/src/store/index.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import photos from "./modules/photos";
 import socket from "./modules/socket";
 
 Vue.use(Vuex);
+
 export default new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    photos,
     socket
   }
 });
