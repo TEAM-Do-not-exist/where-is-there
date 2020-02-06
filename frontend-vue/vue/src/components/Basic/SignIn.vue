187b443cfa6148cbb7688d9d4a67b76cf08472e9
sunghana92 <sunghana92@gmail.com>
2020-02-06 11:12:50 +0900

    front login

--- frontend-vue/vue/src/components/Basic/SignIn.vue
+++ frontend-vue/vue/src/components/Basic/SignIn.vue
@@ -46,7 +46,7 @@
                 >
                   <NaverLogin
                     client-id="nsrxqIjEGhhBf9jdPBFD"
-                    callback-url="http://192.168.100.52:8080/naversignin"
+                    callback-url="http://localhost:8080/naversignin"
                     v-bind:is-popup="false"
                     v-bind:button-type="3"
                     v-bind:button-height="60"
