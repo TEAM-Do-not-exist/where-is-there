c7f49c45e3656a0d003e814cfbce598999cc1bd8
ohjiyun <orora5411@naver.com>
2020-02-06 13:58:10 +0900

    main.js

--- frontend-vue/vue/src/main.js
+++ frontend-vue/vue/src/main.js
@@ -5,7 +5,7 @@ import VueMasonry from "vue-masonry-css";
 import Directives from "./plugins/directives";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
-
+import store from "./store"
 import App from "./App.vue";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
@@ -19,5 +19,6 @@ new Vue({
   router,
   vuex,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount("#app");
