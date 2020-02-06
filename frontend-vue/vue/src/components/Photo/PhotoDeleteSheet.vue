bbe4966488a5b8b4e91fba406952be04009303a1
Myeonghan-Jeong <ataraxia9408@gmail.com>
2020-02-06 14:34:08 +0900

    S02P13C107-59 add photo delete in admin logic

--- frontend-vue/vue/src/components/Photo/PhotoDeleteSheet.vue
+++ frontend-vue/vue/src/components/Photo/PhotoDeleteSheet.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="text-center">
-    <v-bottom-sheet v-model="sheet" persistent>
-      <template v-slot:activator="{ on }">
-        <v-btn class="ma-2" outlined dark color="red darken-1" v-on="on">delete</v-btn>
-      </template>
-      <v-sheet class="text-center" height="200px">
-        <div class="py-12">정말로 삭제하시겠습니까?</div>
-        <v-btn class="mt-6" small text color="red" @click="deletePhoto">delete</v-btn>
-        <v-btn class="mt-6" small text color="blue" @click="sheet = !sheet">close</v-btn>
-      </v-sheet>
-    </v-bottom-sheet>
-  </div>
+  <v-bottom-sheet v-model="sheet" persistent>
+    <template v-slot:activator="{ on }">
+      <v-btn class="ma-2" outlined dark color="red darken-1" v-on="on">delete</v-btn>
+    </template>
+    <v-sheet class="text-center" height="200px">
+      <div class="py-12">정말로 삭제하시겠습니까?</div>
+      <v-btn class="mt-6" small text color="red" @click="deletePhoto">delete</v-btn>
+      <v-btn class="mt-6" small text color="blue" @click="sheet = !sheet">close</v-btn>
+    </v-sheet>
+  </v-bottom-sheet>
 </template>
 
 <script>
