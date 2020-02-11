5777a27db1eb4e577d652e977d0bcc6f894d8f1d
sunghana92 <sunghana92@gmail.com>
2020-02-11 11:08:20 +0900

    repair rest api

--- backend-spring/src/main/java/com/ssafy/project1/authorization/JwtInterceptor.java
+++ backend-spring/src/main/java/com/ssafy/project1/authorization/JwtInterceptor.java
@@ -1,14 +1,21 @@
 package com.ssafy.project1.authorization;
 
+import java.util.Collection;
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.ssafy.project1.exception.UnauthorizedException;
 
+@CrossOrigin(origins= {"*"},maxAge=6000)
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
 	
@@ -21,9 +28,18 @@ public class JwtInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// TODO Auto-generated method stub
+		//content-type
 		final String token = request.getHeader(HEADER_AUTH);
-		System.out.println("token? "+token);
-		if(token !=null && jwtService.isUsable(token)) {
+//		Enumeration<String> attributeNames = request.getHeaderNames();
+//		while(attributeNames.hasMoreElements()) {
+//			String nextElement = attributeNames.nextElement();
+//			String str = request.getHeader(nextElement);
+//			System.out.println(nextElement+" ::: "+str);
+//		}
+		if(request.getHeader("access-control-request-headers")!=null) {
+			return true;
+		}
+		if(token !=null && jwtService.isUsable(token) ) {
 			return true;
 		}
 		else {
